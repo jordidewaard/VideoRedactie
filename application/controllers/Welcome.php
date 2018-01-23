76feb095087b4d8500612cd9cc3cf4f652a3d6da
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2018-01-23 14:03:53 +0100

    URL Helper toegevoegd, Style veranderd, Layout veranderd (Mapnamen) en button toegevoegd om morgen mee te testen

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,6 +20,8 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$this->load->view('index.html');
+		$this->load->view('welcomepage.php');
+        $this->load->helper('url');
 	}
+
 }
---
+++ application/views/welcomepage.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link href="<?php echo base_url(); ?>public/css/style.css" rel="stylesheet">
+    <title>Hello</title>
+</head>
+<body>
+
+<button type="button">Faka Button</button>
+
+</body>
+</html>
