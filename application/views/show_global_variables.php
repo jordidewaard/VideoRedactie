0e36c445f62dc6b32bc8fb182f9f42951d19b8dd
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2018-01-31 09:42:50 +0100

    veranderingetje gemaakt

--- application/views/show_global_variables.php
+++ application/views/show_global_variables.php
@@ -1,5 +1,11 @@
 <?php
 echo "Title of the blog post : ".$title;
 echo "<a href='$web_Address'>"."Click here to go to blog page"."</a>";
+
+In [3]: whos
+Variable   Type    Data/Info
+----------------------------
+foo        str     bar
+
 ?>
 
