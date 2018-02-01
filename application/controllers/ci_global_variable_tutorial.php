b86d6c14daaa139d5ec36fef5eedd9835ffbca96
LAPTOP-4A79OJ3C\jordi <jordidewaard97@gmail.com>
2018-02-01 09:44:41 +0100

    globale variabeles aangepast + Contact form veranderd

--- application/controllers/ci_global_variable_tutorial.php
+++ application/controllers/ci_global_variable_tutorial.php
@@ -1,11 +1,1 @@
-<?php
-class CI_Global_Variable_Tutorial extends CI_Controller{
-    public function __construct() {
-        parent::__construct();
-    }
-// Load view page
-    public function index() {
-        $this->load->view('show_global_variables');
-    }
-}
-?>
+// Globale variable aanwezig //
